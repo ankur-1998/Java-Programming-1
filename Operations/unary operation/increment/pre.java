be0749a6ed414cea38ab736ecfdc17a62f638a36
Ankur Ankit <37009542+ankur-1998@users.noreply.github.com>
2020-03-11 17:40:55 +0530

    Add files via upload

---
+++ Operations/unary operation/increment/pre.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class pre{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a number:");
+		int a;
+		a = sc.nextInt();
+		System.out.println("Value of a = "+a);
+		System.out.println("Pre-Increment of a= "+ (++a));
+		System.out.println("Value of a = "+a);
+		
+	}
+}
