5851b6f9f642ea49c94c2cdbbca7b8e6b44c28bd
Ankur Ankit <37009542+ankur-1998@users.noreply.github.com>
2020-03-16 09:43:34 +0530

    Add files via upload

---
+++ Strings/Sample.java
@@ -0,0 +1,10 @@
+package com.stringprogram.example;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Strings/TrimExampleCode.java
@@ -0,0 +1,8 @@
+package com.stringprogram.example;
+
+public class TrimExampleCode {
+	public static void main(String agrs[]){
+		System.out.print("Welcome to java");
+		System.out.println();
+	}
+}
