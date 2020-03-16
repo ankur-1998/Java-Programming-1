5c47a923631b04382ea8ab373c670718f6fbc235
Ankur Ankit <37009542+ankur-1998@users.noreply.github.com>
2020-03-16 09:52:28 +0530

    Add files via upload

---
+++ Strings/Strings.java
@@ -0,0 +1,15 @@
+package com.String;
+
+public class Strings {
+	public static void main(String args[]){
+		String name = "Ankur";
+		name.concat("Ankit");
+		System.out.println(name);
+		System.out.println(name.concat(" Ankit"));
+		System.out.println(name);
+		
+		String name1 = "Akul";
+		name1 = name1.concat(" Sharma");
+		System.out.println(name1);
+	}
+}
